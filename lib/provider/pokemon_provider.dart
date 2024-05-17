671481dac4061a7096f88a7cb5c4c5cbee02f07b
Anshu Kumar Singh <thisissidam@gmail.com>
2024-05-17 20:38:01 +0530

    Fixed small bug
    Bug: When a base variant pokemon's data is added from the next time that id is used, the base variant is what we'll get, not any other varaints.
    
    Solution: Stopped fetching using speciesId, it was getting the species in the first time loading data but from second time, it was directly pulling from database. Hence, changing variants part was sent to the pokemon_provider.dart file.

--- lib/provider/pokemon_provider.dart
+++ lib/provider/pokemon_provider.dart
@@ -1,17 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:guessthegyarados/database/pokemon_data_db.dart';
 import 'package:guessthegyarados/pokemon_class/pokemon.dart';
 import 'package:guessthegyarados/utils/fetch_data/fetch_data.dart';
 
 final pokemonFutureProvider = FutureProvider.family<Pokemon?, int>((ref, randomId) async {
-  Pokemon? pokemon = PokemonDB.getData(randomId);
+  // Fetch Pokemon from Database; null if not in database
+  Pokemon? pokemonBaseVariant = PokemonDB.getData(randomId);
 
-  if (pokemon != null) return pokemon;
-  debugPrint("id: $randomId");
-  pokemon = await fetchPokemonDataFromSpeciesId(randomId);
-  PokemonDB.addData(randomId, pokemon);
-  
-  return pokemon;
+  // Fetch from pokeApi if not in database and add in database
+  pokemonBaseVariant ??= await fetchPokemonFromPokemonId(randomId);
+  PokemonDB.addData(randomId, pokemonBaseVariant);
+
+  // Get the id of the final chosen variant.
+  final variantIdList = pokemonBaseVariant.variantIDs;
+  final finalId = variantIdList[Random().nextInt(variantIdList.length)];
+
+  // Return base variant if it is final
+  if (finalId == randomId) return pokemonBaseVariant;
+
+  // Else get the final variant's details and return
+  Pokemon? finalPokemon = PokemonDB.getData(finalId);
+
+  if (finalPokemon != null) return finalPokemon;
+
+  finalPokemon = await fetchPokemonFromPokemonId(finalId);
+  return finalPokemon;
 });
 
--- lib/utils/fetch_data/fetch_data.dart
+++ lib/utils/fetch_data/fetch_data.dart
@@ -1,46 +1,8 @@
 import 'dart:convert';
-import 'dart:math';
 import 'package:guessthegyarados/consts/api_links.dart';
 import 'package:guessthegyarados/database/pokemon_data_db.dart';
 import 'package:guessthegyarados/pokemon_class/pokemon.dart';
-import 'package:guessthegyarados/utils/misc_methods.dart';
 import 'package:http/http.dart' as http;
-
-Future<Pokemon> fetchPokemonDataFromSpeciesId(int pokemonId) async {
-
-  Pokemon? pokemon = PokemonDB.getData(pokemonId);
-
-  if (pokemon != null) return pokemon;
-
-  final speciesUrl = Uri.parse('$pokemonSpeciesDataApiLink$pokemonId');
-  final speciesResponse = await http.get(speciesUrl);
-  if (speciesResponse.statusCode == 200) {
-    final speciesData = json.decode(speciesResponse.body);
-    final varieties = speciesData['varieties'] as List<dynamic>;
-
-    // Filter out varieties with '-mega', '-gmax', '-totem' in the name
-    final filteredVarieties = varieties.where((variety) {
-      final varietyName = variety['pokemon']['name'];
-      return !excludePokemon(varietyName);
-    }).toList();
-
-    // Randomly select one variety from filtered list.
-    final random = Random();
-    final selectedVariety = filteredVarieties[random.nextInt(filteredVarieties.length)];
-
-    final pokemonUrl = Uri.parse(selectedVariety['pokemon']['url']);
-    final pokemonResponse = await http.get(pokemonUrl);
-
-    if (pokemonResponse.statusCode == 200) {
-      final jsonData = json.decode(pokemonResponse.body);
-      return Pokemon.fromJsonAsync(jsonData);
-    } else {
-      throw Exception('Failed to fetch Pokemon data');
-    }
-  } else {
-    throw Exception('bbFailed to fetch Pokemon species data');
-  }
-}
 
 Future<Pokemon> fetchPokemonFromPokemonId(int id) async {
 
